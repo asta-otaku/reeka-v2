23ead5dfc47676b3572c97087389d2565071269d
asta-otaku <afolabiibrahim08@gmail.com>
2024-08-29 12:39:52 +0100

    feat: Central admin

--- src/util.tsx
+++ src/util.tsx
@@ -4,9 +4,8 @@ export const CONSTANT = {
   BASE_URL: import.meta.env.VITE_BASE_URL,
   get USER_ID() {
     if (!cachedUserId) {
-      // const user = JSON.parse(localStorage.getItem("user") || "{}");
-      // cachedUserId = user._id || null;
-      cachedUserId = "4d17d412-7344-4ffd-b893-b1ecaf35b0ea";
+      const user = JSON.parse(localStorage.getItem("user") || "{}");
+      cachedUserId = user._id || null;
     }
     return cachedUserId;
   },
