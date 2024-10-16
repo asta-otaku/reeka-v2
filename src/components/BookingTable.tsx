64740a4922ede2b630ffad78899998b80ea724ec
Ayoola Personal <aoluwanusin@gmail.com>
2024-10-16 09:19:50 -0400

    Fixed price per night

--- src/components/BookingTable.tsx
+++ src/components/BookingTable.tsx
@@ -243,7 +243,7 @@ function Modal({ booking, setModal }: { booking: any; setModal: any }) {
           <div>
             <h2 className="text-[#808080] text-xs">Price per night</h2>
             <h4 className="text-[#121212] text-xs mt-0.5">
-              ₦{booking?.totalBookingValue}
+              ₦{booking?.totalBookingValue / booking?.nightsBooked}
             </h4>
           </div>
           <div>
