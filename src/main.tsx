53a72a2b46882dc05d1d2821efc110007640161b
asta-otaku <afolabiibrahim08@gmail.com>
2025-03-04 10:42:02 +0100

    build: fix?

--- src/main.tsx
+++ src/main.tsx
@@ -6,25 +6,25 @@ import { Toaster } from "react-hot-toast";
 import "./index.css";
 
 import App from "./pages/index.tsx";
-import Bookings from "./pages/dashboard/Bookings.tsx";
-import Calendar from "./pages/dashboard/Calendar.tsx";
-import Dashboard from "./pages/dashboard/index.tsx";
-import Integrations from "./pages/dashboard/Integrations.tsx";
-import ListingManagement from "./pages/dashboard/ListingManagement.tsx";
-import ReportCenter from "./pages/dashboard/ReportCenter.tsx";
+import Bookings from "./pages/Dashboard/Bookings.tsx";
+import Calendar from "./pages/Dashboard/Calendar.tsx";
+import Dashboard from "./pages/Dashboard/index.tsx";
+import Integrations from "./pages/Dashboard/Integrations.tsx";
+import ListingManagement from "./pages/Dashboard/ListingManagement.tsx";
+import ReportCenter from "./pages/Dashboard/ReportCenter.tsx";
 import Reservation from "./pages/Reservation.tsx";
 import ViewProperty from "./components/dashboard/viewProperty/index.tsx";
-import SignIn from "./pages/auth/SignIn.tsx";
-import SignUp from "./pages/auth/SignUp.tsx";
+import SignIn from "./pages/Auth/SignIn.tsx";
+import SignUp from "./pages/Auth/SignUp.tsx";
 import Pricing from "./pages/Pricing.tsx";
-import ResetPassword from "./pages/auth/Reset-password.tsx";
-import ForgotPassword from "./pages/auth/Forgot-password.tsx";
-import Settings from "./pages/dashboard/Settings.tsx";
+import ResetPassword from "./pages/Auth/Reset-password.tsx";
+import ForgotPassword from "./pages/Auth/Forgot-password.tsx";
+import Settings from "./pages/Dashboard/Settings.tsx";
 import NotFound from "./pages/NotFound.tsx";
 import PublicBooking from "./pages/PublicBooking.tsx";
 import PublicInvoice from "./pages/PublicInvoice.tsx";
-import StripeSuccessPage from "./pages/stripe/Success.tsx";
-import StripeCancelPage from "./pages/stripe/Cancel.tsx";
+import StripeSuccessPage from "./pages/Stripe/Success.tsx";
+import StripeCancelPage from "./pages/Stripe/Cancel.tsx";
 
 const queryClient = new QueryClient();
 
