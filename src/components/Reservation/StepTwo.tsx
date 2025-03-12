95b8e4e1872c1a4aec277e7fc6c0dea72981ccbe
asta-otaku <afolabiibrahim08@gmail.com>
2025-03-12 12:43:35 +0100

    time offset and signup reordering

--- src/components/Reservation/StepTwo.tsx
+++ src/components/Reservation/StepTwo.tsx
@@ -5,18 +5,7 @@ import { useState } from "react";
 import Spinner from "../Spinner";
 import apiClient from "../../helpers/apiClient";
 import { useCurrency } from "../../helpers/getCurrency";
-
-function formatTimestamp(timestamp: string) {
-  const date = new Date(timestamp);
-
-  // Extract date components
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, "0");
-  const day = String(date.getDate()).padStart(2, "0");
-
-  // Combine to get the desired format
-  return `${year}-${month}-${day}`;
-}
+import moment from "moment";
 
 function StepTwo({
   formDetails,
@@ -58,6 +47,10 @@ function StepTwo({
   });
   const [loading, setLoading] = useState(false);
   const currency = useCurrency();
+
+  function formatTimestamp(date: string) {
+    return moment(date).tz("Africa/Lagos").format("YYYY-MM-DD");
+  }
 
   const handleReserve = async () => {
     setFormData({
