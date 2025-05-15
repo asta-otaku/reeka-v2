25aadea636207e322f16a73cff7845e3b7a11720
asta-otaku <afolabiibrahim08@gmail.com>
2025-05-15 17:27:16 +0100

    chore: updated price to defaultRate

--- src/components/ViewProperty/index.tsx
+++ src/components/ViewProperty/index.tsx
@@ -264,8 +264,9 @@ function ViewProperty() {
                 <span className="w-2 h-2 rounded-full bg-[#808080]" />
                 <span>
                   {currency}
-                  {property?.defaultRate?.ratePrice} per night (
-                  {property?.defaultRate?.rateName})
+                  {property?.defaultRate?.ratePrice ||
+                    property.price.basePrice}{" "}
+                  per night ({property?.defaultRate?.rateName || ""})
                 </span>
               </div>
             </div>
